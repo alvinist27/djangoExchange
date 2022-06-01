3faa654076ceb9acd8fc7b7e0ec322c80259bbdc
alvinist27 <suhrobhotamov496@gmail.com>
2022-06-01 15:58:29 +0300

    Замена SECRET KEY

--- djangoExchange/settings.py
+++ djangoExchange/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)4gsmbn&tat((f@)xgob7s!m@t5qr1y%p@+)=*qui%(y7ms=+^'
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
