00af376cc38c25c7e8b211012d7ded5c07e91b6b
alvinist27 <suhrobhotamov496@gmail.com>
2022-06-01 16:46:06 +0300

    Добавляение тестов для конвертера

--- app_exchange/tests.py
+++ app_exchange/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ConverterTest(TestCase):
+    def test_converter_at_location(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_exchange/convert.html')
+
+    def test_converter_post_request(self):
+        response = self.client.post(reverse('convert'), {
+            'amount': 1200, 'from_choice': 'EUR', 'to_choice': 'USD'
+        })
+        self.assertEqual(response.status_code, 200)
