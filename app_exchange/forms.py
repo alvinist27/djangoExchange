9f7fbcc12b0e1ccbe1eda1f3f217a6d06f278769
alvinist27 <suhrobhotamov496@gmail.com>
2022-06-01 14:07:18 +0300

    Добавление ограничений для формы

--- app_exchange/forms.py
+++ app_exchange/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ConverterForm(forms.Form):
-    amount = forms.FloatField()
+    amount = forms.FloatField(min_value=0, required=True)
     from_choice = forms.ChoiceField(label='From', initial='USD', required=True)
     to_choice = forms.ChoiceField(label='To', initial='EUR', required=True)
